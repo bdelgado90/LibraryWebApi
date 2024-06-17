d71af7552872bb4cf1e945faaf1b79cc8b0663f1
Byron Delgado <bdelgado90@outlook.com>
2024-06-16 23:12:31 -0600

    improve dapper injection

--- LibraryWebApi/Program.cs
+++ LibraryWebApi/Program.cs
@@ -5,25 +5,18 @@ using LibraryWebApi.Services.Interfaces;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddTransient<IDapperWrapper, DapperWrapper>(provider =>
-{
-    var configuration = provider.GetRequiredService<IConfiguration>();
-    return new DapperWrapper(configuration.GetConnectionString("DefaultConnection"));
-});
+var connectionString = builder.Configuration.GetConnectionString("Library");
+builder.Services.AddTransient<IDapperWrapper>(_ => new DapperWrapper(connectionString));
 builder.Services.AddTransient<IBookRepository, BookRepository>();
 builder.Services.AddScoped<IBookService, BookService>();
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 app.UseSwagger();
 app.UseSwaggerUI();
-
 app.UseHttpsRedirection();
 app.MapControllers();
 app.Run();
